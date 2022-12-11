176a7b38b67cd347beb59d2c79201d1144b746b3
DESKTOP-ROJ4E6A\Usuario <jmc1005@alu.ubu.es>
2022-12-11 19:27:50 +0100

    Crear clase estadistica#16

---
+++ ClassLibrary1/Estadistica.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    internal class Estadistica
+    {
+        //
+        private string listaValores;
+
+        // Getters y Setters
+        public string ListaValores
+        {
+            get { return listaValores; }
+            set { listaValores = value; }
+        }
+
+        public Estadistica(string listaValores)
+        {
+            ListaValores = listaValores;
+        }
+
+
+    }
+}
